77f1de45ac3dc4a46481e95162ff46e1544f7638
Martin Najjar <martin_najjar@yahoo.com>
2021-03-16 22:23:50 +0300

    Generate Measure model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_183230) do
+ActiveRecord::Schema.define(version: 2021_03_16_192222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "measures", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_measures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -43,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_03_16_183230) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "measures", "users"
 end
