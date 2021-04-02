88dc5530572aa50258b5d12409761e271897ae02
Martin Najjar <martin_najjar@yahoo.com>
2021-04-02 11:36:43 +0300

    Automatically create measures for the user once he's registered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  after_create :create_measures
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +10,16 @@ class User < ActiveRecord::Base
 
   has_many :measurements
   has_many :measures
+
+  private
+  def create_measures
+    carbohydrates = measures.build(name: 'carbohydrates')
+    proteins = measures.build(name: 'proteins')
+    fats = measures.build(name: 'fats')
+    calories = measures.build(name: 'calories')
+    carbohydrates.save
+    proteins.save
+    fats.save
+    calories.save
+  end
 end
