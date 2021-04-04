7f886bcc37166d99c70dc849a53d634e016b8d2a
Martin Najjar <martin_najjar@yahoo.com>
2021-04-04 18:56:39 +0300

    Use the current user time zone

--- app/controllers/api/v1/measurements_controller.rb
+++ app/controllers/api/v1/measurements_controller.rb
@@ -16,9 +16,11 @@ class Api::V1::MeasurementsController < ApplicationController
     calory_object = current_user.measures.find_by(name: 'calories')
     calories = current_user.measurements.where('measure_id = ?', calory_object.id).order('DATE(created_at) DESC').group('DATE(created_at)').sum(:value)
     results = []
-    calories.each do |key, value|
-      hash = { created_at: key, value: value }
-      results << hash
+    if calories
+      calories.each do |key, value|
+        hash = { created_at: key, value: value }
+        results << hash
+      end
     end
 
     render json: results
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_zone_from_session
 
   protected
 
@@ -8,4 +9,7 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :height, :weight, :activity])
   end
 
+  def set_zone_from_session
+    Time.zone = ActiveSupport::TimeZone[session[:timezone_offset]] if session[:timezone_offset]
+  end
 end
