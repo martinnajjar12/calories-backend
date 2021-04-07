14730eaf33f5d2af4843bb1b139219f762c6163d
Martin Najjar <martin_najjar@yahoo.com>
2021-04-07 21:13:49 +0300

    Modify routes file

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,4 +1,6 @@
 class Measure < ApplicationRecord
   has_many :measurements
   belongs_to :user
+
+  validates_uniqueness_of :name, scope: :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :measures
+      get '/measures', to: 'measures#index'
       get '/calories', :to => 'measurements#show_calories'
     end
   end
