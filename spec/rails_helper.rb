1b70c8d648ec37dda40c77d37bd26eebe75f5f2d
Martin Najjar <martin_najjar@yahoo.com>
2021-04-13 02:31:02 +0300

    Fix rubocop errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,11 +45,11 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with :truncation
     DatabaseCleaner.strategy = :transaction
   end
-  
+
   config.before(:each) do
     DatabaseCleaner.start
   end
-  
+
   config.after(:each) do
     DatabaseCleaner.clean
   end
--- spec/requests/measurements_controller_spec.rb
+++ spec/requests/measurements_controller_spec.rb
@@ -34,7 +34,8 @@ RSpec.describe Api::V1::MeasurementsController, type: :request do
   private
 
   def login
-    post user_session_path, params:  { email: @current_user.email, password: 'password' }.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    post user_session_path, params: { email: @current_user.email, password: 'password' }.to_json,
+                            headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
   end
 
   def get_auth_params_from_login_response_headers(response)
@@ -44,14 +45,12 @@ RSpec.describe Api::V1::MeasurementsController, type: :request do
     token_type = response.headers['token-type']
     uid = response.headers['uid']
 
-    auth_params = {
+    {
       'access-token' => token,
       'client' => client,
       'uid' => uid,
       'expiry' => expiry,
       'token-type' => token_type
     }
-    auth_params
   end
-
 end
