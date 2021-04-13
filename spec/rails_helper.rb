f76b228633ddaae6b75abfc6ced669f46c93fa54
Martin Najjar <martin_najjar@yahoo.com>
2021-04-13 18:26:20 +0300

    Separate helper methods (login and auth_params) from the tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require './spec/support/factory_bot'
+require './spec/support/controller_spec_helper'
 require 'database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -53,6 +54,8 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+  config.include ControllerSpecHelper
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
---
+++ spec/support/controller_spec_helper.rb
@@ -0,0 +1,22 @@
+module ControllerSpecHelper
+  def login
+    post user_session_path, params: { email: @current_user.email, password: 'password' }.to_json,
+                            headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+  end
+
+  def get_auth_params_from_login_response_headers(response)
+    client = response.headers['client']
+    token = response.headers['access-token']
+    expiry = response.headers['expiry']
+    token_type = response.headers['token-type']
+    uid = response.headers['uid']
+
+    {
+      'access-token' => token,
+      'client' => client,
+      'uid' => uid,
+      'expiry' => expiry,
+      'token-type' => token_type
+    }
+  end
+end
